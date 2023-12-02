ce027404c25e7fbc9bc91660e476baac5708f22e
saran-mani <saranmvdm@gmail.com>
2023-12-02 16:58:18 +0530

    delete log

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -17,7 +17,6 @@ module.exports.createCampground = async (req, res, next) => {
   }));
   campground.author = req.user._id;
   await campground.save();
-  console.log(campground);
   req.flash("success", "Successfully made a new campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
@@ -50,7 +49,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  console.log(req.body);
+
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
@@ -67,7 +66,7 @@ module.exports.updateCampground = async (req, res) => {
     await campground.updateOne({
       $pull: { images: { filename: { $in: req.body.deleteImages } } },
     });
-    console.log(campground);
+
   }
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
