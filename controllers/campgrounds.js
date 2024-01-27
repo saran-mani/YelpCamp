569ca3e2c7c89eade773fb3d00c93171da74714b
Saran <57593599+saran-mani@users.noreply.github.com>
2024-01-27 15:18:34 +0530

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -2,7 +2,6 @@ const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
   res.send(campgrounds)
 };
 
